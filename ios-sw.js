b6598f7c8c2dccf28a089c61c67518060e71e12d
abdoali07979-collab <abdoali07979@gmail.com>
2026-09-22 17:41:01 +0300

    Add files via upload

--- ios-sw.js
+++ ios-sw.js
@@ -1,15 +1,15 @@
-const CACHE='manara-ios-v6100';
+const CACHE='manara-ios-v6110';
 const FILES=[
-  './ios-app.html?v=6100',
-  './ios-style.css?v=6100',
-  './ios-supabase.js?v=6100',
-  './ios-config.js?v=6100',
-  './ios-app.js?v=6100',
-  './ios-manifest.webmanifest?v=6100',
-  './logo.jpg?v=6100',
-  './apple-touch-icon.png?v=6100',
-  './icon-192.png?v=6100',
-  './icon-512.png?v=6100'
+  './ios-app.html?v=6110',
+  './ios-style.css?v=6110',
+  './ios-supabase.js?v=6110',
+  './ios-config.js?v=6110',
+  './ios-app.js?v=6110',
+  './ios-manifest.webmanifest?v=6110',
+  './logo.jpg?v=6110',
+  './apple-touch-icon.png?v=6110',
+  './icon-192.png?v=6110',
+  './icon-512.png?v=6110'
 ];
 
 self.addEventListener('install', event => {
